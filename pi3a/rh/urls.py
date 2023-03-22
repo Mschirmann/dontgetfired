da3e0c18a81fceb10bacbd1acdf3665a7313d1b4
Mauricio Schirmann <mauricio.schirmann@gmail.com>
2023-03-21 22:55:30 -0300

    form validations

--- pi3a/rh/urls.py
+++ pi3a/rh/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from rh.views import UserListView, UserDetailView, TimesheetListView
+from rh.views import (
+    UserListView,
+    UserDetailView,
+    TimesheetListView,
+    TimesheetRegisterView,
+)
 
 
 app_name = "rh"
 urlpatterns = [
     path("user/list", UserListView.as_view(), name="users"),
-    path("user/detail/<int:pk>", UserDetailView.as_view(), name="user_detail"),
+    path("user/form/<int:pk>", UserDetailView.as_view(), name="user_form"),
     path("timesheet/list", TimesheetListView.as_view(), name="timesheet_list"),
+    path(
+        "timesheet/register", TimesheetRegisterView.as_view(), name="timesheet_register"
+    ),
 ]
--- pi3a/rh/views.py
+++ pi3a/rh/views.py
@@ -1,5 +1,52 @@
-from django.views.generic import TemplateView, DetailView, ListView
+import datetime
+from django.views.generic import TemplateView, DetailView, ListView, View
 from .models import User, Time_sheet, Holidays, Vacations
+from django.http import HttpResponse
+
+
+class UserProfileView(TemplateView):
+    context_object_name = "user_profile"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["vacations"] = self.get_user_vacation()
+        context["last_registers"] = self.get_recently_user_timesheet()
+        context["holiday"] = self.get_holiday()
+        return context
+
+    def get_user_vacation(self):
+        if self.request.user.is_authenticated:
+            # get last vacation register
+            vacation = (
+                Vacations.objects.filter(fk_user=self.request.user, confirmed=True)
+                .order_by("-id")
+                .last()
+            )
+            return vacation
+        return None
+
+    def get_recently_user_timesheet(self):
+        if self.request.user.is_authenticated:
+            # get last 5 timesheet register
+            last_registers = Time_sheet.objects.filter(
+                fk_user=self.request.user
+            ).order_by("-created_at")[:5][::-1]
+            return last_registers
+        return None
+
+    def get_holiday(self):
+        today = datetime.datetime.today()
+        current_year = today.year
+        current_month = today.month
+        current_day = today.day
+
+        holidays = Holidays.objects.filter(
+            year=current_year, month=current_month, day=current_day
+        )
+
+        if holidays.exists():
+            return holidays.last()
+        return None
 
 
 class UserRegisterView(TemplateView):
@@ -29,9 +76,13 @@ class TimesheetListView(ListView):
         return queryset
 
 
-class TimesheetRegisterView(TemplateView):
-    model = Time_sheet
-    template_name = "rh/timesheet_form.html"
+class TimesheetRegisterView(View):
+    def post(self, request, *args, **kwargs):
+        print(request.user)
+        print(request.POST["register"])
+        Time_sheet.objects.create(fk_user=request.user)
+
+        return HttpResponse(True, status=201)
 
 
 class VacationListView(ListView):
